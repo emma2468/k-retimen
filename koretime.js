dc955b6a0d0cd66e02301e11a2ca63162ef2b2ea
Emma Ryom <emmaryom@Emmas-MBP.lan>
2018-04-15 23:11:24 +0200

    youlose

--- koretime.js
+++ koretime.js
@@ -1,6 +1,7 @@
 $(window).on("load", scenenVises);
 
-$("#startHistorie").on("click", walkSlut);
+$("#scenenVises").on("click", walkSlut);
+$("#loseVises").on("click", loseVises);
 
 function scenenVises(){
     console.log("scenenVises");
@@ -10,14 +11,12 @@ function scenenVises(){
     $("#tekstboks_container").addClass("tekstboks_container_out");
     $("#knapja_container").addClass("knapja_container_out");
     $("#knapnej_container").addClass("knapnej_container_out");
+    $("#youlose_container").addClass("youlose_container_out");
 }
 
 function walkSlut(){
     console.log("walkSlut");
 
-<<<<<<< HEAD
-    $()
-=======
     $("#tekstboks_container").removeClass("tekstboks_container_out");
     $("#knapja_container").removeClass("knapja_container_out");
     $("#knapja_container").removeClass("knapja_container_out");
@@ -27,7 +26,21 @@ function walkSlut(){
     $("#knapnej_container").addClass("slide_nej");
     $("#tekstboks_container").addClass("tekstboks_container_in");
     $("#knapja_container").addClass("knapja_container_in");
-    $("#knapja_container").addClass("knapja_container_in");
+    $("#knapnej_container").addClass("knapnej_container_in");
+    $("#youlose_container").addClass("youlose_container_out");
+}
 
->>>>>>> koeretime/master
+function loseVises(){
+    console.log("loseVises");
+
+    $("#tekstboks_container").removeClass("slide");
+    $("#knapja_container").removeClass("slide_ja");
+    $("#knapnej_container").removeClass("slide_nej");
+    $("#tekstboks_container").removeClass("tekstboks_container_in");
+    $("#knapja_container").removeClass("knapja_container_in");
+    $("#knapnej_container").removeClass("knapnej_container_in");
+    $("#youlose_container").addClass("youlose_container_out");
+
+    $("#youlose_container").addClass("slide_yoylose");
+    $("youlose_container").addClass("youlose_container_in");
 }
